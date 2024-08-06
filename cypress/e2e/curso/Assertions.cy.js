98bab502ba457486cd423f0e705a7c3b43f89cad
Sahueson <ivancabanillas93@gmail.com>
2024-08-06 19:50:55 +0200

    Reubicacion del curso y primer test x-tap

--- cypress/e2e/curso/Assertions.cy.js
+++ cypress/e2e/curso/Assertions.cy.js
@@ -16,4 +16,19 @@ describe("Assertions demo", () => {
 
 
     });
-});
+
+    it('Explicit assertions', () => {
+
+        cy.visit("https://portal-dev.x-tap.club/login")
+        cy.get("input#input-email").type("ivan.c@alphaxperience.io")    // id tag is optional
+        cy.get("input#input-password").type("xtap123")
+        cy.get("button").click()
+
+        let expName = "xyz";
+
+        cy.get("h1").then((x) => {
+            let actName = x.text()
+
+        });
+    });
+})
